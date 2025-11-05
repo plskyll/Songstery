d2bdcaff9df42d0294cdf194470ddf7fa092d42a
plskyll <yuliia.v.plysiuk@ukd.edu.ua>
2025-11-05 23:36:44 +0200

    Initial commit. Add a home page and simple model Book

---
+++ core/views.py
@@ -0,0 +1,92 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+books = [
+    {
+        'id': 1,
+        'title': 'Шістка воронів',
+        'author': 'Лі Бардуго',
+        'cover': 'https://static.yakaboo.ua/media/catalog/product/1/1/116_12_1.jpg',
+        'genre': 'Фентезі',
+        'year': 2015,
+    },
+    {
+        'id': 2,
+        'title': 'Тюремна цілителька',
+        'author': 'Лінетт Ноні',
+        'cover': 'https://static.yakaboo.ua/media/cloudflare/product/webp/600x840/7/_/7_36_2.jpg',
+        'genre': 'Фентезі',
+        'year': 2021,
+    },
+    {
+        'id': 3,
+        'title': 'Золота клітка',
+        'author': 'Лінетт Ноні',
+        'cover': 'https://static.yakaboo.ua/media/catalog/product/7/9/79_30.jpg',
+        'genre': 'Фентезі',
+        'year': 2021,
+    },
+]
+
+popular_music = [
+    {
+        'id': 1,
+        'title': "Trouble",
+        'artist': 'Valerie Broussard',
+        'book': 'Шістка воронів',
+        'meta': 'Valerie Broussard • Шістка воронів',
+        'likes': 2458,
+    },
+    {
+        'id': 2,
+        'title': 'Survivor',
+        'artist': '2WEI',
+        'book': 'Тюремна цілителька',
+        'meta': '2WEI • Тюремна цілителька',
+        'likes': 1892,
+    },
+    {
+        'id': 3,
+        'title': 'Castle',
+        'artist': 'Halsey',
+        'book': 'Золота клітка',
+        'meta': 'Halsey • Золота клітка',
+        'likes': 1673,
+    },
+    {
+        'id': 4,
+        'title': 'Way Down We Go',
+        'artist': 'KALEO',
+        'book': 'Шістка воронів',
+        'meta': 'KALEO • Шістка воронів',
+        'likes': 1521,
+    },
+    {
+        'id': 5,
+        'title': 'Lovely',
+        'artist': 'Billie Eilish, Khalid',
+        'book': 'Тюремна цілителька',
+        'meta': 'Billie Eilish, Khalid • Тюремна цілителька',
+        'likes': 1384,
+    },
+    {
+        'id': 6,
+        'title': 'Everybody Wants To Rule The World',
+        'artist': 'Lorde',
+        'book': 'Золота клітка',
+        'meta': 'Lorde • Золота клітка',
+        'likes': 1256,
+    },
+]
+
+# Create your views here.
+class HomeView(TemplateView):
+    template_name = 'core/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Головна'
+        context['section_title'] = 'Популярні книги'
+        context['books'] = books
+        context['popular_music'] = popular_music
+        return context
