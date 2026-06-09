70df16ab7fe0f606e1250ac6c09804cd944a8aad
plskyll <yuliia.v.plysiuk@ukd.edu.ua>
2026-06-09 15:50:04 +0300

    Fixed deployment variables in buid sh

--- build.sh
+++ build.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+python manage.py shell << 'PYEOF'
+from django.contrib.auth import get_user_model
+User = get_user_model()
+import os
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
+email    = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', '')
+if password and not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+    print(f'Superuser "{username}" created.')
+else:
+    print(f'Superuser "{username}" already exists or password not set.')
+PYEOF
